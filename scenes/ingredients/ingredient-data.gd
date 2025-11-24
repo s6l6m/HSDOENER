b726199dd8d5b795bb53a20037865c2a8ba859b2
Salam <117642092+s6l6m@users.noreply.github.com>
2025-11-24 16:37:05 +0100

    52 food manager npc manager and UI connection (#53)
    * further added the base for the food manager
    
    - added ingredients as data structure
    - added make doener func in ordermanager
    
    * random doener generator
    
    * npcs und ordermanager verbunden
    
    ---------
    
    Co-authored-by: 1nfamousL <132676152+1nfamousL@users.noreply.github.com>

---
+++ scenes/ingredients/ingredient-data.gd
@@ -0,0 +1,7 @@
+class_name Ingredient
+extends Resource
+
+@export var name: String # z.b. tomate, gurke etc.
+# @export var category: String
+# @export var price: float = 0.0
+@export var icon: Texture2D = null
--- scenes/orders/order_manager.gd
+++ scenes/orders/order_manager.gd
@@ -4,18 +4,10 @@ signal order_added(order)
 signal order_completed(order)
 signal order_removed(order)
 
+@onready var doner_generator: DonerGenerator = $"doener-generator"
+
+
 var orders: Array[Order] = []
-
-func create_order(customer: Node, items: Array[String]) -> Order:
-	var order = Order.new()
-	order.id = orders.size() + 1
-	order.customer = customer
-	order.items = items
-	order.is_completed = false
-
-	orders.append(order)
-	emit_signal("order_added", order)
-	return order
 
 func complete_order(order: Order):
 	if order in orders:
@@ -26,3 +18,16 @@ func remove_order(order: Order):
 	if order in orders:
 		orders.erase(order)
 		emit_signal("order_removed", order)
+
+func create_doner_order(customer: Node) -> Order:
+	var ingredients := doner_generator.generate_doner()
+
+	var order = Order.new()
+	order.id = orders.size() + 1
+	order.customer = customer
+	order.ingredients = ingredients
+	order.is_completed = false
+
+	orders.append(order)
+	emit_signal("order_added", order)
+	return order
