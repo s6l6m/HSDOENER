e39718dbd3328bd80f361d06134f91960c18b066
1nfamousL <132676152+1nfamousL@users.noreply.github.com>
2025-12-04 15:54:10 +0100

    teller klasse

--- scenes/orders/teller.gd
+++ scenes/orders/teller.gd
@@ -1,11 +1,5 @@
-extends Node2D
+# teller.gd
+class_name Teller
+extends Area2D    # Root deines Teller-Nodes
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var order: Order = null
